815e71c84debd747aed019cf3eabef2c3ff92030
sivaram2662 <106672296+sivaram2662@users.noreply.github.com>
2023-05-05 09:31:20 +0530

    fixed deployment

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 unzip wordpress.zip 
 rm -fr /var/www/html/*
 sudo cp -r * /var/www/html
-rm -fr *.zip *.php wp-admin wp-content wp-includes
+rm -fr *.zip *.php wp-admin wp-content wp-includes *.html
